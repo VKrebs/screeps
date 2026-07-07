abf43cdfa584d69ba4dd466083d2111ba03b4be3
valentin@vkrebs.fr <valentin@vkrebs.fr>
2026-07-07 23:23:18 +0200

    feat(spawn) : first spwaner

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import roleHarvester from "./role.harvester";
 import roleUpgrader from "./role.upgrader";
 import roleBuilder  from "./role.builder";
+import * as ld from 'lodash';
 
 declare global {
     interface CreepMemory {
@@ -15,6 +16,28 @@ declare global {
 }
 
 export function loop() {
+
+    // Clear non-existing creeps memory
+    for (const name in Memory.creeps) {
+        if (!Game.creeps[name]) {
+            delete Memory.creeps[name];
+        }
+    }
+
+    var harvesters = ld.filter(Game.creeps, (creep) => creep.memory.role == 'harvester')
+    var upgraders = ld.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader')
+    var builders = ld.filter(Game.creeps, (creep) => creep.memory.role == 'builder')
+
+    if (harvesters.length < 2) {
+        var newName = 'Harvester-' + Game.time;
+        Game.spawns["Spawn1"]?.spawnCreep([WORK, CARRY, MOVE, MOVE], newName, {memory: {role: 'harvester', state: undefined}})
+    } else if (upgraders.length < 2) {
+        var newName = 'Upgrader-' + Game.time;
+        Game.spawns["Spawn1"]?.spawnCreep([WORK, CARRY, MOVE, MOVE], newName, {memory: {role: 'upgrader', state: undefined}})
+    } else if (builders.length < 1) {
+        var newName = 'Builder-' + Game.time;
+        Game.spawns["Spawn1"]?.spawnCreep([WORK, CARRY, MOVE, MOVE], newName, {memory: {role: 'builder', state: undefined}})
+    }
 
     for (const name in Game.creeps) {
         const creep = Game.creeps[name]!;
